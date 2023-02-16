bb7c79be9550d773544319107a96b130cccd3277
Nazanin-Shahrad <nazanin.shahrad@gmail.com>
2023-02-16 11:25:57 -0500

    hotelRoute.js completed

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-
+import authRoute  from './routes/authRoute.js';
+import hotelsRoute from './routes/hotelsRoute.js';
+import roomsRoute from './routes/roomsRoute.js';
+import usersRoute from './routes/usersRoute.js'
+const app = express();
 dotenv.config();
 
 const connect = async() => {
@@ -13,7 +17,16 @@ const connect = async() => {
     throw error
  }
 }
-const app = express();
+
+
+
+//middleware
+app.use(express.json());
+
+app.use('/api/auth' , authRoute)
+app.use('/api/hotels' , hotelsRoute)
+app.use('/api/rooms' , roomsRoute)
+app.use('/api/users' , usersRoute)
 
 app.listen(4000 ,() => {
     connect();
