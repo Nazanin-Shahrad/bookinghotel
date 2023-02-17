0da347337447d42e66de500fd84df32d29ba964f
Nazanin-Shahrad <nazanin.shahrad@gmail.com>
2023-02-17 09:42:59 -0500

    rooms logic added in roomController , room routes add added

--- server/routes/roomsRoute.js
+++ server/routes/roomsRoute.js
@@ -1,7 +1,23 @@
 import express from "express";
+import {createRoom ,deleteRoom ,getRoom,getRooms , updateRoom} from '../controllers/roomControllers.js';
+import { verifyAdmin , verifyUser } from "../utils/verifyToken.js";
 const router = express.Router();
 
 
+//CREATE
+router.post("/:hotelid" ,verifyAdmin, createRoom)
 
+//UPDATE
+router.put("/:id" ,verifyAdmin, updateRoom)
+
+//DELETE
+router.delete("/:id/:hotelid" , verifyAdmin, deleteRoom)
+
+//GET
+router.get("/:id" , getRoom)
+
+
+//GET ALL
+router.get("/" , getRooms)
 
 export default router;
