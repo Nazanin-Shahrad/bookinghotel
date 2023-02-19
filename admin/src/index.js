c6447c0424c56e0d5b2ef4720157575d17bad67c
Nazanin-Shahrad <nazanin.shahrad@gmail.com>
2023-02-18 19:01:26 -0500

    just copied from project I have done few weeks ago bookingwebsite project admin part

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { AuthContextProvider } from "./context/AuthContext";
+import { DarkModeContextProvider } from "./context/darkModeContext";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <AuthContextProvider>
+      <DarkModeContextProvider>
+        <App />
+      </DarkModeContextProvider>
+    </AuthContextProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
 
